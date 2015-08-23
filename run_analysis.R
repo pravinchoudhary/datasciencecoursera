30eea9e08fdee1caeeb17cbb9926618623fe5c0f
Pravin K Choudhary <Pravin.Choudhary@itcinfotech.com>
2015-08-24 04:41:44 +0530

    Course Project - Getting and Cleaning Data

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 ## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./projectDataset.zip")
 
 ## Read train and test subject/X/y data files
-read.table("./UCI HAR Datasettrain/subject_train.txt")
+read.table("./UCI HAR Dataset/train/subject_train.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
@@ -19,7 +19,6 @@ X_combined <- rbind(X_train, X_test)
 y_combined <- rbind(y_train, y_test)
 names(y_combined) <- c("activity")
 
-
 ## Step 2: Following code will fine all mean and std columns / features but somehow
 ##         its not working while summarizing
 
@@ -29,8 +28,10 @@ names(y_combined) <- c("activity")
 ##         taking first 6 sample of mean and std columns
 sample_X_combined <- X_combined[, 1:6]
 
-## Step 3: Putting descriptive activity names to name the activities in the data set
-factor(y_combined[, "activity"], labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+## Step 3: y_combinedPutting descriptive activity names to name the activities in the data set
+y_combined <- factor(y_combined[, "activity"], labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+y_combined <- as.data.frame(y_combined)
+names(y_combined) <- c("activity")
 
 ## combine subject, feature and activit into one
 data_combined <- cbind(subject_combined, sample_X_combined, y_combined)
@@ -38,6 +39,7 @@ data_combined <- cbind(subject_combined, sample_X_combined, y_combined)
 ## Step 4: set labels the data set with descriptive variable names
 ## Step 5: data set with the average of each variable for each activity and each subject
 tidy_data <- group_by(data_combined, activity)
+tidy_data <- group_by(data_combined, subject)
 tidy_data <- summarise(output, tBodyAccXMean = mean(V1, na.rm = TRUE),
                        tBodyAccYMean = mean(V2, na.rm = TRUE),
                        tBodyAccZMean = mean(V3, na.rm = TRUE),
