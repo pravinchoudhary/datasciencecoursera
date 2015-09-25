306c99e6c1f28520d5316de9be650a8167e5fa2d
Pravin K Choudhary <Pravin.Choudhary@itcinfotech.com>
2015-09-26 01:56:08 +0530

    Getting and Cleaning Data - Course Project

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 ## Data Science - "Getting and Cleaning Data" - Course Project
+This document describes the code inside `run_analysis.R`. Purpose or objective of the 'R' script is to read the data as provided and then creating a single tidy data set out of multiple input data sets containg subject, feature variables and label.
 
-## Steps to execute the 'R' script
+### Load zip files and extract them. input file paths are used as  relative path Vs. absolute path
 
-* download the input data file
-## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./projectDataset.zip")
+### Reading and creting one dataset per subject, training and label variables
 
-* Extract zip file in the same directory as the script run_analysis.R
+Merge following datset (row wise)
 
-* Run the sctipt, it will dump a sample tidy data file by name 'sample_tidy_data.txt'
+* `UCI HAR Dataset/train/X_train.txt` and `UCI HAR Dataset/test/X_test.txt`
+* `UCI HAR Dataset/train/y_train.txt` and `UCI HAR Dataset/teset/y_test.txt`
+* `UCI HAR Dataset/train/subject_train.txt` and `UCI HAR Dataset/test/subject_test.txt`
 
-## enjoy!
+
+## Label data variables accordingly 
+   * subject
+   * Feature names as per 'features.txt' file content
+   * activity
+
+## Data filter and cleanup
+   * Filter only those columns having "mean" and "std" (standard daviation data variables)
+   * Remove unwanted characters from the label
+
+## creating a single dataset
+using column bind creat a single dataset set containing
+   * subject data
+   * feature variables (X)
+   * label (y)
+at this point of thme we will have 10299 records of data
+
+## final tidy dataset
+   * Group the merged single data set by "activity" & subject. 
+   * summarize data using mean function for each of the feature variables
+
+we are using "group_by" & "summarise" function of dplyr library. after this step we should have 180 records of cleaned and trasfored data sets.
+
+
+## Writing final data to a local file
+dump data final tidy dataset to a txt  file using table
+Creates the output dir if it doesn't exist and writes `meanAndStdAverages` data frame to the ouputfile.
--- run_analysis.R
+++ run_analysis.R
@@ -1,50 +1,53 @@
-## download the input data file
-## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./projectDataset.zip")
-
-## Read train and test subject/X/y data files
-read.table("./UCI HAR Dataset/train/subject_train.txt")
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-
-
-## Step 1: merge and create one data file
-subject_combined <- rbind(subject_train, subject_test)
-names(subject_combined) <- c("subject")
-
-X_combined <- rbind(X_train, X_test)
-y_combined <- rbind(y_train, y_test)
-names(y_combined) <- c("activity")
-
-## Step 2: Following code will fine all mean and std columns / features but somehow
-##         its not working while summarizing
-
-##read.table("./features.txt")
-##feature <- read.table("./features.txt")
-##X_combined <- X_combined[sort(c(grep("std", colnames(X_combined)), grep("mean", colnames(X_combined)))), ]
-##         taking first 6 sample of mean and std columns
-sample_X_combined <- X_combined[, 1:6]
-
-## Step 3: y_combinedPutting descriptive activity names to name the activities in the data set
-y_combined <- factor(y_combined[, "activity"], labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
-y_combined <- as.data.frame(y_combined)
-names(y_combined) <- c("activity")
-
-## combine subject, feature and activit into one
-data_combined <- cbind(subject_combined, sample_X_combined, y_combined)
-
-## Step 4: set labels the data set with descriptive variable names
-## Step 5: data set with the average of each variable for each activity and each subject
-tidy_data <- group_by(data_combined, activity)
-tidy_data <- group_by(data_combined, subject)
-tidy_data <- summarise(output, tBodyAccXMean = mean(V1, na.rm = TRUE),
-                       tBodyAccYMean = mean(V2, na.rm = TRUE),
-                       tBodyAccZMean = mean(V3, na.rm = TRUE),
-                       tBodyAccXStd = mean(V4, na.rm = TRUE),
-                       tBodyAccYStd = mean(V5, na.rm = TRUE),
-                       tBodyAccZStd = mean(V5, na.rm = TRUE))
-
-write.table(tidy_data, "./sample_tidy_data.txt")
+run_analysis <- function() {
+    ## load required libraries
+    library(dplyr)
+    library(tidyr)
+    
+    ## download the input data file
+    ## download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./projectDataset.zip")
+    
+    ## Read train and test subject/X/y data files
+    read.table("./UCI HAR Dataset/train/subject_train.txt")
+    subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+    X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+    y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+    subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+    X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+    y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+    
+    
+    ## In sthis part we are creating one file each for each "subject", 
+    ## "Feature/X"and "Label/y" (Step 1)
+    subject_combined <- rbind(subject_train, subject_test)
+    X_combined <- rbind(X_train, X_test)
+    y_combined <- rbind(y_train, y_test)
+    
+    names(subject_combined) <- c("subject")
+    names(y_combined) <- c("activity")
+    
+    ## Labeling feature variables in a descriptive manner using information
+    ## in "features.txt" (Step 4)
+    ## Also cleaning up unwanted charaters (e.g. '(')
+    feature <- read.table("./UCI HAR Dataset/features.txt")
+    tmp <- as.vector(feature[ , 2])
+    tmp <- gsub("[()]", "", tmp)
+    names(X_combined) <- tmp
+    
+    ## Extract only mean and standard deviation features (Step 2)
+    X_combined_mean_std <- X_combined[ ,grep("mean|std", names(X_combined))]
+    
+    ## Use descriptive activity names to name the activities in the data set (Step 3)
+    y_combined <- as.data.frame(factor(y_combined[, "activity"], labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")))
+    names(y_combined) <- c("activity")
+    
+    ## combine subject, feature and activit into one (Step 1)
+    data_combined <- cbind(subject_combined, X_combined_mean_std, y_combined)
+    
+    ## Create a independent data set with average of each variable 
+    ## for each activity and each subject (Step 5)
+    tidy_data <- group_by(data_combined, activity, subject)
+    tidy_data <- summarise_each(tidy_data, funs(mean))
+    
+    ## Dump the tidy data set to a file
+    write.table(tidy_data, "./sample_tidy_data.txt", row.names = FALSE)
+}
